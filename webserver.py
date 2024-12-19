77fc7a85f6081809947f67cb0515ab2a93dd6d7d
mack.wtf <mackamuir@gmail.com>
2024-12-19 19:26:55 +1100

    Change from sending to websocket to send directly to file

--- webserver.py
+++ webserver.py
@@ -1,57 +1,63 @@
-# combined_server.py
 import asyncio
 import websockets
 from aiohttp import web
 import json
 import PySSM2
 import time
+import csv
 
-# WebSocket handler to send multiple values to connected clients
-async def websocket_handler(websocket):
+# SSM2 Handler to log data from the ECU
+async def start_ssm2_logger():
+    # Initalize our CSV File
+    start_time = time.time()
+    timestr = time.strftime('%Y%m%d-%H%M%S')
+    logfile = open('%s-SubaruLog.csv' % timestr, 'w+')
+    logfileheaders = ['Time', 'Boost Pressure', 'Manifold Absolute Pressure', 'Atmospheric Pressure', 'Coolant Temperature', 'A/F Sensor #1', 'Air Fuel Ratio', 'Mass Airflow', 'Vehicle Speed', 'Engine Speed', 'Battery Voltage', 'Fuel Consumption', 'Engine Load']
+    lf = csv.DictWriter(logfile, fieldnames= logfileheaders)
+    lf.writeheader()
     # Initialize PySSM2
     SSM2 = PySSM2.PySSM2('/dev/ttyUSB0')
     #Initalize the ECU
     SSM2.ecu_init()
-    # execution_count = 0
-    # start_time = time.time()
-    # interval = 1
     # Request our data from the ECU Continously 
     # Battery Voltage, Coolant Temperature, A/F Sensor #1, Manifold Absolute Pressure, Atmospheric Pressure, Vehicle Speed, Mass Airflow, Engine Speed
     addresses = [0x00001C, 0x000008, 0x000046, 0x00000D, 0x000023, 0x000010, 0x000013, 0x00000E]
     SSM2.read_single_address(addresses)
-    try:
-        while True:
-            # execution_count += 1
-            # elapsed_time = time.time() - start_time
-            # if elapsed_time >= interval:
-            #     executions_per_second = execution_count / elapsed_time
-            #     print(f"Executions per second: {executions_per_second}")
-            #     start_time = time.time()
-            #     execution_count = 0
-            # We request the length of our address list and then 6 for heading and checksum
-            response = SSM2.receive_packet(len(addresses) + 6)
-            # print(f"{' '.join(hex(n) for n in response)}")
-            battVolt = round(response[5] * 0.08, 1)
-            coolent = response[6] - 40
-            afr = round(response[7] / 128 * 14.7, 2)
-            boost = round((response[8] * 37 / 255) - (response[9] * 37 / 255), 1)
-            if response[10] == 0:
-                fuelCon = 0
-            else:
-                fuelCon = (3600/response[10]) * ((response[11] / afr )/761)*100
-            if response[12] == 0:
-                engineLoad = 0
-            else:
-                engineLoad = (response[11]*60) / response[12] 
-            data = {
-                'Boost': boost,
-                'CoolentTemp': coolent,
-                'BatteryVoltage': battVolt,
-                'AirFuel': afr,
-                'FuelConsumption': fuelCon,
-                'EngineLoad': engineLoad,
-            }
+    while True:
+        # We request the length of our address list and then add 6 for heading and checksum bytes
+        response = SSM2.receive_packet(len(addresses) + 6)
+        battVolt = round(response[5] * 0.08, 1)
+        coolent = response[6] - 40
+        afr = round(response[7] / 128 * 14.7, 2)
+        boost = round((response[8] * 37 / 255) - (response[9] * 37 / 255), 1)
+        if response[10] == 0:
+            fuelCon = 0
+        else:
+            fuelCon = (3600/response[10]) * ((response[11] / afr )/761)*100
+        if response[12] == 0:
+            engineLoad = 0
+        else:
+            engineLoad = (response[11]*60) / response[12] 
+        data = {
+            'Time': time.time() - start_time,
+            'Boost Pressure': boost,
+            'Manifold Absolute Pressure': response[8],
+            'Atmospheric Pressure': response[9],
+            'Coolant Temperature': coolent,
+            'A/F Sensor #1': response[7],
+            'Air Fuel Ratio': afr,
+            'Mass Airflow': response[11],
+            'Vehicle Speed': response[10],
+            'Engine Speed': response[12],
+            'Battery Voltage': battVolt,
+            'Fuel Consumption': fuelCon,
+            'Engine Load': engineLoad
+        }
+        lf.writerow(data)
 
+
+# WebSocket handler to send multiple values to connected clients
+async def websocket_handler(websocket):
             # Send the JSON data to the client
             await websocket.send(json.dumps(data))
 
@@ -61,12 +67,12 @@ async def websocket_handler(websocket):
             # Wait for 1 second before sending the next set of values
             
 
-    except websockets.ConnectionClosedOK:
-        print("Client disconnected cleanly")
-    except websockets.ConnectionClosedError:
-        print("Connection closed with error")
-    except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+    # except websockets.ConnectionClosedOK:
+    #     print("Client disconnected cleanly")
+    # except websockets.ConnectionClosedError:
+    #     print("Connection closed with error")
+    # except Exception as e:
+    #     print(f"An unexpected error occurred: {e}")
 
 # Start WebSocket server
 async def start_websocket_server():
@@ -96,6 +102,7 @@ async def start_http_server():
 # Main function to run both servers concurrently
 
 async def main():
+    ssm2_task = asyncio.create_task(start_ssm2_logger())
     websocket_task = asyncio.create_task(start_websocket_server())  # WebSocket server
     http_task = asyncio.create_task(start_http_server())           # HTTP server
     await asyncio.gather(websocket_task, http_task)
